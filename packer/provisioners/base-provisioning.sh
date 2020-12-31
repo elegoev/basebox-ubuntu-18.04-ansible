f70bc8e1f469df0aadfa9049d89ba25afbf06c2e
Urs Voegele <unext@bluewin.ch>
2020-12-31 13:52:49 +0100

    new basebox version 3.0.1609418584

--- packer/provisioners/base-provisioning.sh
+++ packer/provisioners/base-provisioning.sh
@@ -1,5 +1,7 @@
 ##!/bin/bash
 
+application_file_path="/vagrant/installed-application.md"
+
 # install ansible
 sudo apt-add-repository ppa:ansible/ansible
 sudo apt update
@@ -18,4 +20,6 @@ sudo apt install ansible -y
 sudo echo "export ANSIBLE_CONFIG='./ansible.cfg'" >> /home/vagrant/.bashrc
 
 ANSIBLE_VERSION=$(ansible --version | sed -n 1p | grep ansible | awk  '{print $2}')
-# echo "ansible-$ANSIBLE_VERSION" > /vagrant/version
+echo "# Installed application   "  > $application_file_path
+echo "***                       " >> $application_file_path
+echo "> ansible $ANSIBLE_VERSION" >> $application_file_path
